7d95c288997cc8c5a02371f4cfc65f036bc12bc4
thor7777777 <thor7777777@mail.ru>
2021-11-26 13:09:19 +0300

    dz_1

--- dz_1/1/main.py
+++ dz_1/1/main.py
@@ -1,16 +1,22 @@
-# This is a sample Python script.
+duration = int(input('duration = '))
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if duration >= 0 and duration < 60:
+    print(f'{duration} сек')
+elif duration >= 0 and duration < 3600:
+    time_min = duration // 60
+    time_sec = duration % 60
+    print(f'{time_min} мин {time_sec} сек')
+elif duration >= 0 and duration < 86400:
+    time_hour = duration // 3600
+    time_min = (duration % 3600) // 60
+    time_sec = (duration % 3600) % 60
+    print(f'{time_hour} час {time_min} мин {time_sec} сек')
+elif duration >= 0 and duration >= 86400:
+    q_day = duration // 86400
+    difference = duration - q_day * 86400
+    time_hour = difference // 3600
+    time_min = (difference % 3600) // 60
+    time_sec = (difference % 3600) % 60
+    print(f'{q_day} дн {time_hour} час {time_min} мин {time_sec} сек')
+else:
+    print('Неверные данные')
